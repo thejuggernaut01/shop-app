2897521fa4840b710b7f23fe78c12fb81db9a1a7
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2023-10-06 07:51:06 +0100

    handled postAddProduct error

--- app.js
+++ app.js
@@ -52,10 +52,16 @@ app.use((req, res, next) => {
 
   User.findById(req.session.user._id)
     .then((user) => {
+      if (!user) {
+        return next();
+      }
+
       req.user = user;
       next();
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      throw new Error(err);
+    });
 });
 
 app.use((req, res, next) => {
@@ -68,6 +74,8 @@ app.use((req, res, next) => {
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
+
+app.get("/500", errorController.get500);
 
 app.use(errorController.get404);
 
