450dde2f9fb2201c450cf96a272e799cd59a8228
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2023-10-18 07:23:07 +0100

    improved how pdf is being served to the client

--- controllers/shop.js
+++ controllers/shop.js
@@ -152,28 +152,37 @@ exports.getOrders = (req, res, next) => {
 exports.getInvoice = (req, res, next) => {
   const orderId = req.params.orderId;
 
-  Order.findById(orderId).then((order) => {
-    if (!order) {
-      return next(new Error("No order found."));
-    }
-
-    if (order.user.userId !== req.user._id) {
-      return next(new Error("Unauthorized"));
-    }
-
-    const invoiceName = "invoice-" + orderId + ".pdf";
-    const invoicePath = path.join("data", "invoices", invoiceName);
-
-    fs.readFile(invoicePath, (err, data) => {
-      if (err) {
-        return next(err);
+  Order.findById(orderId)
+    .then((order) => {
+      if (!order) {
+        return next(new Error("No order found."));
       }
+
+      if (order.user.userId !== req.user._id) {
+        return next(new Error("Unauthorized"));
+      }
+
+      const invoiceName = "invoice-" + orderId + ".pdf";
+      const invoicePath = path.join("data", "invoices", invoiceName);
+
+      // fs.readFile(invoicePath, (err, data) => {
+      //   if (err) {
+      //     return next(err);
+      //   }
+      // res.setHeader("Content-Type", "application/pdf");
+      // res.setHeader(
+      //   "Content-Disposition",
+      //   'attachment; filename="' + invoiceName + '" '
+      // );
+      //   res.send(data);
+      // });
+      const file = fs.createReadStream(invoicePath);
       res.setHeader("Content-Type", "application/pdf");
       res.setHeader(
         "Content-Disposition",
         'attachment; filename="' + invoiceName + '" '
       );
-      res.send(data);
-    });
-  });
+      file.pipe(res);
+    })
+    .catch((err) => next(err));
 };
