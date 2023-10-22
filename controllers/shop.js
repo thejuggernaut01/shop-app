a06009053ef09097572cd87dbb64d12ccb7ede48
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2023-10-22 04:58:45 +0100

    integrated payment using stripe

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const stripe = require("stripe")("sk_test_Flc1Upp19T0q8ZgmKGDVJUI400j9emUSTr");
 
 const PDFDocument = require("pdfkit");
 
@@ -137,7 +138,54 @@ exports.postCartDeleteProduct = (req, res, next) => {
     });
 };
 
-exports.postOrder = (req, res, next) => {
+exports.getCheckout = (req, res, next) => {
+  let products;
+  let total = 0;
+  req.user
+    .populate("cart.items.productId")
+    // .execPopulate()
+    .then((user) => {
+      products = user.cart.items;
+      total = 0;
+      products.forEach((p) => {
+        total += p.quantity * p.productId.price;
+      });
+
+      return stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: products.map((p) => {
+          return {
+            name: p.product.title,
+            description: p.productId.description,
+            amount: p.productId.price * 100,
+            currency: "usd",
+            quantity: p.quantity,
+          };
+        }),
+        success_url:
+          req.protocol + "://" + req.get("host") + "/checkout/success",
+        cancel_url: req.protocol + "://" + req.get("host") + "/checkout/cancel",
+      });
+    })
+    .then((session) => {
+      res.render("shop/checkout", {
+        path: "/checkout",
+        pageTitle: "Checkout",
+        products: products,
+        totalSum: total,
+        sessionId: session.id,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+
+      return next(error);
+    });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
   req.user
     .populate("cart.items.productId")
     // .execPopulate()
--- public/js/admin.js
+++ public/js/admin.js
@@ -2,14 +2,20 @@ const deleteProduct = (btn) => {
   const prodId = btn.parentNode.querySelector("[name=productId]").value;
   const csrf = btn.parentNode.querySelector("[name=_csrf]").value;
 
-  fetch("/admin/product" + prodId, {
+  const parentElement = btn.closest("article");
+
+  fetch("/admin/product/" + prodId, {
     method: "DELETE",
     headers: {
       "csrf-token": csrf,
     },
   })
     .then((result) => {
-      console.log(result);
+      return result.json();
+    })
+    .then((data) => {
+      console.log(data);
+      parentElement.parentNode.removeChild(parentElement);
     })
     .catch((err) => {
       console.log(err);
